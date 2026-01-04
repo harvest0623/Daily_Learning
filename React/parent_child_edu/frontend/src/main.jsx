1928de08877ba3f0e9def8b4eb1090f3a09101fb
harvest0623 <3367741939@qq.com>
2026-01-04 21:48:34 +0800

    program1

---
+++ React/parent_child_edu/frontend/src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './utils/rem.js'
+import './styles/Global.css'
+
+createRoot(document.getElementById('root')).render(
+    <App />
+)
